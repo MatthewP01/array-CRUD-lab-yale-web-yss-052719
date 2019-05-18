a4c91549483eb7c85f7dcbb0d9f71ba834b2315e
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-18 16:50:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   # array = ["gryffindor", "slytherin", "hufflepuff", "ravenclaw"]
-  array = array.pop
+  array_new = array.pop
 end
 
 def remove_element_from_start_of_array(array)
