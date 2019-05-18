dbc304b2b71fd364870f518323fcd3efb6232de5
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-18 16:44:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["gryffindor", "slytherin", "hufflepuff"]
   element = "ravenclaw"
-  array << element
+  array << "ravenclaw"
 end
 
 def add_element_to_start_of_array(array, element)
